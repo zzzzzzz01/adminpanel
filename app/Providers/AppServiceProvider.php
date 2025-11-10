52546b60cef2841b32f38587c9d15611b04d222a
Shohjaxon Xurramov <khrrmvsh@gmail.com>
2025-11-10 19:22:39 +0500

    ssss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-        }
+        
     }
 }
